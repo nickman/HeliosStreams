29179be68db90376ca846646fca5d4a1102df56a
nickman <whitehead.nicholas@gmail.com>
2016-07-03 11:31:32 -0400

    DevSynch

--- stream-hub/src/main/java/com/heliosapm/streams/admin/AdminFinder.java
+++ stream-hub/src/main/java/com/heliosapm/streams/admin/AdminFinder.java
@@ -16,16 +16,19 @@
 package com.heliosapm.streams.admin;
 
 import java.nio.charset.Charset;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.apache.curator.CuratorZookeeperClient;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.RetrySleeper;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.imps.CuratorFrameworkState;
 import org.apache.curator.framework.listen.Listenable;
 import org.apache.curator.framework.recipes.cache.ChildData;
 import org.apache.curator.framework.recipes.cache.TreeCache;
@@ -52,9 +55,25 @@ import com.heliosapm.utils.jmx.JMXManagedThreadFactory;
  * <p>Company: Helios Development Group LLC</p>
  * @author Whitehead (nwhitehead AT heliosdev DOT org)
  * <p><code>com.heliosapm.streams.admin.AdminFinder</code></p>
+AdminFinder:
+============
+start connect loop
+on connect
+	create treecache (FILTER)
+	if url not present, listen on url
+	on url drop latch
+
+listen on node removed/changed
+	if removed, start listener
+	if changed, update and reset app
+
  */
 
 public class AdminFinder implements Watcher, RetryPolicy {
+	/** The singleton instance */
+	private static volatile AdminFinder instance = null;
+	/** The singleton instance ctor lock */
+	private static final Object lock = new Object();
 	/** The zookeep parent node name to retrieve the streamhub admin url */
 	public static final String ZOOKEEP_URL_ROOT = "/streamhub/admin";
 	/** The zookeep node name to retrieve the streamhub admin url */
@@ -67,21 +86,48 @@ public class AdminFinder implements Watcher, RetryPolicy {
 	public static final String ZOOKEEP_TIMEOUT_ARG = "--ztimeout=";
 	/** The default zookeep session timeout in ms. */
 	public static final int DEFAULT_ZOOKEEP_TIMEOUT = 15000;
-
 	/** The command line arg prefix for the retry pause period in ms. */
 	public static final String RETRY_ARG = "--retry=";
 	/** The default retry pause period in ms. */
 	public static final int DEFAULT_RETRY = 15000;
-	
-	
-	
 	/** The command line arg prefix for the connect retry timeout in ms. */
 	public static final String CONNECT_TIMEOUT_ARG = "--ctimeout=";
 	/** The default zookeep connect timeout in ms. */
 	public static final int DEFAULT_CONNECT_TIMEOUT = 5000;
-	
 	/** The UTF character set */
 	public static final Charset UTF8 = Charset.forName("UTF8");
+	
+	/**
+	 * Initializes and acquires the AdminFinder singleton instance
+	 * @param args The app command line arguments
+	 * @return the AdminFinder singleton instance
+	 */
+	public static AdminFinder getInstance(final String[] args) {
+		if(instance==null) {
+			synchronized(lock) {
+				if(instance==null) {
+					instance = new AdminFinder(args);
+				}
+			}
+		}
+		return instance;
+	}
+	
+	/**
+	 * Acquires the initialized AdminFinder singleton instance
+	 * @return the AdminFinder singleton instance
+	 */
+	public static AdminFinder getInstance() {
+		if(instance==null) {
+			synchronized(lock) {
+				if(instance==null) {
+					throw new IllegalStateException("The AdminFinder has not been initialized");
+				}
+			}
+		}
+		return instance;
+	}
+	
 	
 	/** Instance logger */
 	protected final Logger log = LogManager.getLogger(AdminFinder.class);
@@ -94,16 +140,25 @@ public class AdminFinder implements Watcher, RetryPolicy {
 	/** The zookeep client */
 	protected ZooKeeper zk = null;
 	
+	/** The tree cache instance */
+	protected TreeCache treeCache = null;
+	
+	/** The curator framework */
+	protected CuratorFramework cf = null;
+	
 	/** Indicates if we're connected */
 	protected final AtomicBoolean connected = new AtomicBoolean(false);
+	/** The admin URL */
+	protected final AtomicReference<String> adminURL = new AtomicReference<String>(null);
+	
 	/** The zookeep session id */
 	protected Long sessionId = null;
 	/** The initial connect retry timeout in ms. */
 	protected int connectTimeout = -1;
 	/** The reconnect payse time in ms. */
 	protected int retryPauseTime = -1;
-	
-	protected final ThreadFactory threadFactory = JMXManagedThreadFactory.newThreadFactory("ZooKeeperAdminFiner", true); 
+	/** The thread factry for the curator client */
+	protected final ThreadFactory threadFactory = JMXManagedThreadFactory.newThreadFactory("ZooKeeperAdminFinder", true); 
 	
 
 	/**
@@ -114,10 +169,54 @@ public class AdminFinder implements Watcher, RetryPolicy {
 		zookeepConnect = findArg(ZOOKEEP_CONNECT_ARG, DEFAULT_ZOOKEEP_CONNECT, args);
 		zookeepTimeout = findArg(ZOOKEEP_TIMEOUT_ARG, DEFAULT_ZOOKEEP_TIMEOUT, args);
 		connectTimeout = findArg(CONNECT_TIMEOUT_ARG, DEFAULT_CONNECT_TIMEOUT, args);
-		retryPauseTime = findArg(RETRY_ARG, DEFAULT_RETRY, args);		
+		retryPauseTime = findArg(RETRY_ARG, DEFAULT_RETRY, args);
+		cf = CuratorFrameworkFactory.builder()
+				.canBeReadOnly(false)
+				.connectionTimeoutMs(connectTimeout)
+				.sessionTimeoutMs(zookeepTimeout)
+				.connectString(zookeepConnect)
+				.retryPolicy(new ExponentialBackoffRetry(5000, 200))
+				//.retryPolicy(this)
+				.threadFactory(threadFactory)
+				.build();		
 	}
 	
-	public void start() {
+	/*
+	 * States:
+	 * =======
+	 * cf not started
+	 * cf started / not connected
+	 * cf started / connected
+	 * 
+	 */
+	
+	protected void start() {
+		if(!connected.get()) {
+			log.info("Starting Connection Loop");
+			final CountDownLatch connectLatch = new CountDownLatch(1);
+			threadFactory.newThread(new Runnable(){
+				public void run() {
+					if(cf.getState()==CuratorFrameworkState.STOPPED) {
+						loff();
+						try {
+							cf.start();
+							try {
+								cf.blockUntilConnected();
+								
+							} catch (InterruptedException iex) {
+								log.error("Interupted while waiting on connect", iex);
+							}
+						} finally {
+							lon();
+						}
+					}
+					
+				}
+			}).start();
+		}
+	}
+	
+	public void startx() {
 		try {
 			log.info("Starting Connection Loop");
 //			loff();
